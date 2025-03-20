ee7ed6ddef72e98bcb1ca2b51f96d138d92291cc
JardelBa <jardelba.ads@gmail.com>
2025-03-20 20:39:54 -0300

    Conteudo sobre variaveis

---
+++ variaveis.py
@@ -0,0 +1,10 @@
+# criacao e declaração da variavel 
+#nome = "luka"
+# mostrando mensasgem na tela 
+#print(nome)
+# entrada de dados no computador
+
+nomeCompleto = str(input("Qual é o teu nome?"))
+print(nomeCompleto)
+
+
